822fa6399f86c65baeb4e4abd2b9bd5a3dec88e7
“GabrieldePaula95” <gabrielpauleira@hotmail.com>
2023-08-24 15:22:36 -0300

    atu

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { New } from '../pages/New';
 import { Home } from '../pages/Home/Index';
 import { Profile } from '../pages/Profile';
-import { Details } from '../pages/Details'
+// import { Details } from '../pages/Details';
 
 export function AppRoutes(){
     return(
@@ -11,7 +11,7 @@ export function AppRoutes(){
             <Route path="/" element={<Home />} />
             <Route path="/new" element={<New />} />
             <Route path="/profile" element={<Profile />} />
-            <Route path="/details/:id" element={<Details />} />
+            {/* <Route path="/details/:id" element={<Details />} /> */}
         </Routes>
     );
 }
